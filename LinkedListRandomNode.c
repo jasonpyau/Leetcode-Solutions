38aeeed92890f186dfc3205ce6bd9df4cf0b9535
Jason Yau <jasonyau101@gmail.com>
2023-03-10 13:58:26 -0500

    Update LinkedListRandomNode.c
    382. Linked List Random Node
    https://leetcode.com/problems/linked-list-random-node/description/

--- LinkedListRandomNode.c
+++ LinkedListRandomNode.c
@@ -8,30 +8,32 @@
 
 typedef struct {
     int length;
-    struct ListNode* head;
+    int* values;
 } Solution;
 
 Solution* solutionCreate(struct ListNode* head) {
-    srand(time(NULL));
     Solution* obj = (Solution*)malloc(sizeof(Solution));
+    int* values = (int*)calloc(10000, sizeof(int));
     struct ListNode* current = head;
     int length;
-    for (length = 0; current != NULL; length++)
+    for (length = 0; current != NULL; length++) {
+        values[length] = current->val;
         current = current->next;
+    }
+    values = (int*)realloc(values, length*sizeof(int));
+    obj->values = values;
     obj->length = length;
-    obj->head = head;
+    srand(time(NULL));
     return obj;
 }
 
 int solutionGetRandom(Solution* obj) {
-    int randIndex = rand()%(obj->length);
-    struct ListNode* current = obj->head;
-    for (int i = 0; i < randIndex && current != NULL; i++)
-        current = current->next;
-    return current->val;
+    int randIndex = (obj->length) ? rand()%(obj->length) : 0;
+    return obj->values[randIndex];
 }
 
 void solutionFree(Solution* obj) {
+    free(obj->values);
     free(obj);
 }
 
