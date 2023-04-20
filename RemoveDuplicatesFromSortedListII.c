6cb610c8ec39e6b5e3c5d0999c265cd164aba2ee
Jason Yau <jasonyau101@gmail.com>
2023-04-20 13:44:26 -0400

    Create RemoveDuplicatesFromSortedListII.c
    82. Remove Duplicates from Sorted List II
    https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/

---
+++ RemoveDuplicatesFromSortedListII.c
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* nextNonDupe(struct ListNode* curr) {
+    if (!curr)
+        return NULL;
+    int val = curr->val;
+    while (curr && curr->val == val)
+        curr = curr->next;
+    return curr;
+}
+
+struct ListNode* deleteDuplicates(struct ListNode* head) {
+    while (head && head->next) {
+        if (head->val == head->next->val)
+            head = nextNonDupe(head);
+        else
+            break;
+    }
+    struct ListNode* curr = head;
+    while (curr) {
+        struct ListNode* prev = curr;
+        curr = curr->next;
+        while (curr && curr->next && curr->val == curr->next->val)
+            curr = nextNonDupe(curr);
+        prev->next = curr;
+    }
+    return head;
+}
