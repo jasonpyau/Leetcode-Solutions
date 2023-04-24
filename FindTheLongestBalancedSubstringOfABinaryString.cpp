58937005acd1964010e3b4abc9ead243ac6e80df
Jason Yau <jasonyau101@gmail.com>
2023-04-24 19:35:06 -0400

    Create FindTheLongestBalancedSubstringOfABinaryString.cpp
    2609. Find the Longest Balanced Substring of a Binary String
    https://leetcode.com/problems/find-the-longest-balanced-substring-of-a-binary-string/description/

---
+++ FindTheLongestBalancedSubstringOfABinaryString.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findTheLongestBalancedSubstring(string s) {
+        vector<int> consec;
+        consec.push_back(0);
+        bool lastOne = s[0]-'0';
+        for (int i = 0, consec_i = 0; i < s.size(); i++) {
+            int ch = s[i]-'0';
+            if ((ch && !lastOne) || (!ch && lastOne)) {
+                consec.push_back(0);
+                consec_i++;
+            }
+            consec[consec_i]++;
+            lastOne = ch;
+        }
+        int ans = 0;
+        for (int i = s[0]-'0'+1; i < consec.size(); i += 2) {
+            int curr = 2*min(consec[i-1], consec[i]);
+            if (curr > ans)
+                ans = curr;
+        }
+        return ans;
+    }
+};
