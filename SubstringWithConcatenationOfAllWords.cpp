c6e10f18cffbc43f70742e63a772cf5ec2cb870e
Jason Yau <jasonyau101@gmail.com>
2023-04-25 09:03:50 -0400

    Update SubstringWithConcatenationOfAllWords.cpp
    30. Substring with Concatenation of All Words
    https://leetcode.com/problems/substring-with-concatenation-of-all-words/

--- SubstringWithConcatenationOfAllWords.cpp
+++ SubstringWithConcatenationOfAllWords.cpp
@@ -2,12 +2,8 @@ class Solution {
 public:
     vector<int> findSubstring(string s, vector<string>& words) {
         unordered_map<string, int> word_map;
-        for (string word : words) {
-            auto it = word_map.find(word);
-            if (it == word_map.end())
-                word_map[word] = 1;
-            else
-                it->second++;
+        for (const string& word : words) {
+            word_map[word]++;
         }
         int word_len = words[0].size(), substr_len = words.size()*word_len;
         vector<int> ans;
@@ -19,10 +15,8 @@ public:
                 if (it == word_map_cpy.end()) {
                     break;
                 } else {
-                    it->second--;
-                    if (!it->second) {
+                    if (!(--it->second))
                         word_map_cpy.erase(it);
-                    }
                 }
             }
             if (word_map_cpy.empty())
