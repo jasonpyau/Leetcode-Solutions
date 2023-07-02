e5f7fe3831259bc8f01b59a8d5d7b6c0c9ab4428
Jason Yau <jasonyau101@gmail.com>
2023-07-02 11:55:35 -0400

    Create LongestEvenOddSubarrayWithThreshold.cpp
    2760. Longest Even Odd Subarray With Threshold
    https://leetcode.com/problems/longest-even-odd-subarray-with-threshold/description/

---
+++ LongestEvenOddSubarrayWithThreshold.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestAlternatingSubarray(vector<int>& nums, int threshold) {
+        int curr, ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            while (nums[i]&1 || nums[i] > threshold) {
+                i++;
+                if (i == nums.size()) {
+                    return ans;
+                }
+            }
+            curr = 1;
+            while (i < nums.size()-1 && nums[i+1] <= threshold && (nums[i]&1)^(nums[i+1]&1)) {
+                curr++;
+                i++;
+            }
+            ans = max(ans, curr);
+        }
+        return ans;
+    }
+};
