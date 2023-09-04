4d25c127e3cff95ba8443ad92f7e4c3c753b4672
Jason Yau <jasonyau101@gmail.com>
2023-09-04 11:41:48 -0400

    Create LinkedListCycle.c
    141. Linked List Cycle
    https://leetcode.com/problems/linked-list-cycle/

---
+++ LinkedListCycle.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+typedef struct ListNode ListNode;
+
+bool hasCycle(ListNode *head) {
+    ListNode* slow = head, *fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) {
+            return true;
+        }
+    }
+    return false;
+}
