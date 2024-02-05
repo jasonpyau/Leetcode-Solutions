93be683d5a042f6a2b4b685cf6f4d3dc3574b22c
Jason Yau <jasonyau101@gmail.com>
2024-02-05 16:10:54 -0500

    Create FirstUniqueCharacterInAString.c
    387. First Unique Character in a String
    https://leetcode.com/problems/first-unique-character-in-a-string/

--- FirstUniqueCharacterInAString.c
+++ FirstUniqueCharacterInAString.c
@@ -1,1 +1,12 @@
-
+int firstUniqChar(char* s) {
+    int freq[26] = {0};
+    for (int i = 0; s[i]; ++i) {
+        ++freq[s[i]-'a'];
+    }
+    for (int i = 0; s[i]; ++i) {
+        if (freq[s[i]-'a'] == 1) {
+            return i;
+        }
+    }
+    return -1;
+}
