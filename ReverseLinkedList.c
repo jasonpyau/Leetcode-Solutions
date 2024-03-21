182f03c7593bce9b823e9d5928106e824517b0cf
Jason Yau <jasonyau101@gmail.com>
2024-03-21 11:01:36 -0400

    Create ReverseLinkedList.c
    206. Reverse Linked List
    https://leetcode.com/problems/reverse-linked-list/

---
+++ ReverseLinkedList.c
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* reverseList(struct ListNode* head) {
+    struct ListNode* prev = NULL;
+    struct ListNode* curr = head;
+    while (curr) {
+        struct ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
